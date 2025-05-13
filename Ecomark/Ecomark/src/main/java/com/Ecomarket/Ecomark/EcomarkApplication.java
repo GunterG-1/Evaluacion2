fb6dc2e0fa5e53840527141a2b1b886f9c514e3d
GunterG-1 <gu.garate@duocuc.cl>
2025-05-13 17:29:26 -0400

    Add files via upload

---
+++ Ecomark/Ecomark/src/main/java/com/Ecomarket/Ecomark/EcomarkApplication.java
@@ -0,0 +1,13 @@
+package com.Ecomarket.Ecomark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarkApplication.class, args);
+	}
+
+}
