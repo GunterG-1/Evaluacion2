4eb20f7813bd56f3b496aebcda01f65c9f006300
JosePizarroBerrios <jo.pizarrob@duocuc.cl>
2025-05-15 15:56:31 -0400

    se agregan roles para usuarios y clase dirección para envíos

--- Usuarios/src/main/java/com/usuarios/Usuarios/model/Tarjeta.java
+++ Usuarios/src/main/java/com/usuarios/Usuarios/model/Tarjeta.java
@@ -1,5 +1,8 @@
 package com.usuarios.Usuarios.model;
 
+import java.sql.Date;
+
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,8 +17,28 @@ import lombok.NoArgsConstructor;
 
 public class Tarjeta {
 
-    private int id_usuario;
-    private int numero_Tarjeta;
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_tarjeta;
+
+    @Column(unique = true,length = 16,nullable = false)
+    private String numero_Tarjeta;
     
+    @Column(unique = false,length = 3, nullable = true)
+    private String cvv;
+
+    @Column(unique = false,length = 100,nullable = false)
+    private String nom_titular;
+
+    @Column(unique = false,nullable = false)
+    private Date fecha_vencimiento;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario",nullable = false)
+    private Usuario usuario;
+
+
+
+
 
 }
--- Usuarios/src/main/java/com/usuarios/Usuarios/model/Usuario.java
+++ Usuarios/src/main/java/com/usuarios/Usuarios/model/Usuario.java
@@ -1,5 +1,10 @@
 package com.usuarios.Usuarios.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,23 +23,31 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id_usuario;
+    private long id_usuario;
 
-    @Column(unique = true,length = 25,nullable = false)
+    @Column(unique = false,length = 25,nullable = false)
     private String nom_usuario;
 
     @Column(unique = false,length = 25,nullable = false)
     private String ap_usuario; 
 
-    @Column(unique = false,length = 25,nullable=true)
-    private String dir_usuario;
-
-    @Column(unique=false,length = 25,nullable=false)
-    private String tipo_usuario;
-
     @Column(unique = true,length = 50,nullable = false)
     private String email_Usuario;
-    
 
+    @Column(unique = false,length = 25,nullable = false)
+    private String contraseña_usuario;
+
+    @OneToMany(mappedBy = "usuario",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Direccion> direcciones_usuario = new ArrayList<>();
+
+    @OneToMany(mappedBy = "usuario",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tarjeta> tarjetas_usuario = new ArrayList<>();
     
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "rol",
+        joinColumns = @JoinColumn(name = "id_usuario"),
+        inverseJoinColumns = @JoinColumn(name = "id_rol")
+    )
+    private Set<Rol> roles = new HashSet<>();
 }
